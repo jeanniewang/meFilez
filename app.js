ee0ef2227d0e9b41ca32323ab342006b2e2bac56
Fares <fares.osman@outlook.com>
2021-01-06 23:38:40 -0500

    add nodemon to package.json, start setting up express server

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => res.send("Test"));
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
