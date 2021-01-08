5f63e0f277782f8f7f3de44dd31dd7c1e7946c5a
Fares <fares.osman@outlook.com>
2021-01-08 00:29:05 -0500

    fix connection to mongodb

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
