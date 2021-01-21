6e44456a571511e8f47f4b99ff9b5928e5d26234
Fares <fares.osman@outlook.com>
2021-01-21 00:09:14 -0500

    Configure passport

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,27 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const mongoose = require("mongoose");
 const keys = require("../config/keys");
+const User = require("../models/User");
+const passport = require("passport");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: opts.jwtFromRequest,
+      secretOrKey: opts.secretOrKey,
+    },
+    function (jwtPayload, done) {
+      return User.findById(jwtPayload.sub)
+        .then((user) => {
+          return done(null, user);
+        })
+        .catch((err) => {
+          return done(err);
+        });
+    }
+  )
+);
